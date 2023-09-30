3d2a94d7821cb297f4e8147d3d7ff5d017a635a9
Ed de Feber <84625097+EdF2021@users.noreply.github.com>
2023-09-30 18:21:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,26 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import openai
+from PIL import Image
+
+image = Image.open('images/producttoer.jpeg')
+st.set_page_config(
+        page_title="Berend-Botje Skills",
+        page_icon="👋",
+        layout="wide",
+        initial_sidebar_state="collapsed"
+)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("📖Berend-Botje Skills" )
+    st.subheader("De ChatGPT kloon\n*waarom zou je moeilijk doen ....?*")
+with col2:
+   st.image(image, caption=None, width=240, use_column_width=None, clamp=True, channels="RGB", output_format="png")
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 """
 # Welcome to Streamlit!
