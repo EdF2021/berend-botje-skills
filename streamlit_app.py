00c6e77ba99006361448d9d00e3b8f8f6645479d
Ed de Feber <84625097+EdF2021@users.noreply.github.com>
2023-09-30 20:22:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,6 @@ st.set_page_config(
         layout="wide",
         initial_sidebar_state="collapsed"
 )
-
 col1, col2 = st.columns(2)
 
 with col1:
@@ -23,6 +22,40 @@ with col1:
     st.subheader("De ChatGPT kloon\n*waarom zou je moeilijk doen ....?*")
 with col2:
    st.image(image, caption=None, width=240, use_column_width=True, clamp=True, channels="RGB", output_format="auto")
+
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Hoe gaat het?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+
 
 # openai.api_key = st.secrets["OPENAI_API_KEY"]
 
